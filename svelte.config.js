c6120d85e84c3a6055eaed76adb5d9d8be027ce5
Dhruv Kumar <dhruv.kumar.s17@gmail.com>
2023-08-03 19:11:02 +0100

    directory fixed

---
+++ svelte.config.js
@@ -0,0 +1,37 @@
+import preprocess from "svelte-preprocess";
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//     kit: {
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	},
+
+//     preprocess: [preprocess({
+//         postcss: true
+//     })]
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
+ 
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    })
+  }
+      // preprocess: [preprocess({
+      //     postcss: true
+      // })]
+};
